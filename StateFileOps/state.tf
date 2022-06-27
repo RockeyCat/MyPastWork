a40a50433f907e0965ece10ce4b589e7021bf296
RockeyCat <himanshujoshi378@gmail.com>
2022-06-27 11:30:44 +0530

    DynamoDB integration of stateFile

--- StateFileOps/state.tf
+++ StateFileOps/state.tf
@@ -6,11 +6,12 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "av-aws-cicd-s3-bucket"
-    key    = "network/tmp.tfstate"
+    key    = "network/demo.tfstate"
     region = "us-east-2"
+    dynamodb_table = "av-test-table"
   }
 }
 
 resource "time_sleep" "wait" {
-  create_duration = "150s"
+  create_duration = "60s"
 }
