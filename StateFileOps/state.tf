1622c2f4c604b31080fef3efbe3dabeadf8be7cd
RockeyCat <himanshujoshi378@gmail.com>
2022-06-27 11:09:49 +0530

    pushing the statefileops on my repo

---
+++ StateFileOps/state.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region                   = var.region
+  shared_config_files      = ["/var/root/.aws/config"]
+  shared_credentials_files = ["/var/root/.aws/credentials"]
+}
+
+terraform {
+  backend "s3" {
+    bucket = "av-aws-cicd-s3-bucket"
+    key    = "network/tmp.tfstate"
+    region = "us-east-2"
+  }
+}
+
+resource "time_sleep" "wait" {
+  create_duration = "150s"
+}
