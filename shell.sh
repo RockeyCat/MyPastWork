ae422bee0876d8774470a92fa7a46ef5f60020a1
Himanshu Joshi <himanshujoshi378@gmail.com>
2024-08-16 17:15:51 -0700

    Initial commit with contents of MyPracxTerraform

---
+++ shell.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit with contents of MyPracxTerraform"
+git remote add origin https://github.com/Himanshuuj1997/MyPracxTerraform.git
+git checkout -b DevOpsArch
+git push -u origin DevOpsArch
