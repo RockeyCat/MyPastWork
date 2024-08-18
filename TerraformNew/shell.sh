2cfaff31efc310a310b262243227984b71917bf3
Himanshu Joshi <himanshujoshi378@gmail.com>
2024-08-18 11:33:28 -0700

    ReOrganising the Files again

--- TerraformNew/shell.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit with contents of MyPracxTerraform"
-git remote add origin https://github.com/Himanshuuj1997/MyPracxTerraform.git
-git checkout -b DevOpsArch
-git push -u origin DevOpsArch
